296ea160e903fa0a03e1cb56deda1656abd6c002
Giuseppe Carella <cargius@gmail.com>
2016-02-05 17:12:32 +0100

    Added kurento-repository-server installation scripts
    added the installation script of the kurento-repository-server. The package is
    downloaded from the http://builds.kurento.org/release/stable/ repository

---
+++ start-kurento-repo-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /opt/kurento-repository-server
+screen -d -m -S kurento-repo-server bin/start.sh
--- start-single-mongo.sh
+++ start-single-mongo.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-if [ -z ${PORT+x} ]; then 
-  echo "PORT is unset";
-  PORT=27018
-else 
-  echo "PORT is set to '$PORT'"; 
-fi
-
-if [ -z ${SECURITY+x} ]; then 
-  echo "SECURITY is unset";
-  SECURITY="false"
-else 
-  echo "SECURITY is set to '$SECURITY'";
-fi
-
 function kill {
     if screen -list | grep "mongodb"; then
         screen -ls | grep mongodb | cut -d. -f1 | awk '{print $1}' | xargs kill
@@ -23,53 +9,39 @@ function kill {
 mkdir /home/ubuntu/data
 export LC_ALL=C
 
-if [ $SMALLFILES = 'true' ]
+if [ $smallfiles = 'true' ]
 then
-	screen -d -m -S mongodb mongod --smallfiles --port $PORT --dbpath /home/ubuntu/data
+	screen -d -m -S mongodb mongod --smallfiles --port 27018 --dbpath /home/ubuntu/data
 else
-	screen -d -m -S mongodb mongod --port $PORT --dbpath /home/ubuntu/data
+	screen -d -m -S mongodb mongod --port 27018 --dbpath /home/ubuntu/data
 fi
 
-while ! nc -z localhost $PORT; do
+while ! nc -z localhost 27018; do
   sleep 0.5 # wait for 1/10 of the second before check again
 done
 
+echo "use admin" >> addUser.js
+echo "db.createUser( { user: '`echo $USERNAME_MD`', pwd: '`echo $PASSWORD`', roles: [ 'userAdminAnyDatabase' ] } )" >> addUser.js
 
-if [ $SECURITY = 'true' ]
+cat addUser.js
+
+mongo --port 27018 < addUser.js
+
+kill
+
+sleep 3
+
+if [ $smallfiles = 'true' ]
 then
-  
-  if [ -z ${USERNAME_MD+x} ]; then
-    echo "USERNAME_MD needs to be set if SECURITY is set to true"
-    exit 99
-  fi
-  if [ -z ${PASSWORD+x} ]; then
-    echo "PASSWORD needs to be set if SECURITY is set to true"
-    exit 98
-  fi
-
-  echo "use admin" >> addUser.js
-  echo "db.createUser( { user: '`echo $USERNAME_MD`', pwd: '`echo $PASSWORD`', roles: [ 'userAdminAnyDatabase' ] } )" >> addUser.js
-
-  cat addUser.js
-
-  mongo --port $PORT < addUser.js
-
-  kill
-
-  sleep 3
-
-  if [ $SMALLFILES = 'true' ]
-  then
-  	echo "executing screen"
-  	screen -d -m -S mongodb mongod --smallfiles --port $PORT --dbpath /home/ubuntu/data --auth
-  else
-  	screen -d -m -S mongodb mongod --port $PORT --dbpath /home/ubuntu/data --auth
-  fi
+	echo "executing screen"
+	screen -d -m -S mongodb mongod --smallfiles --port 27018 --dbpath /home/ubuntu/data --auth
+else
+	screen -d -m -S mongodb mongod --port 27018 --dbpath /home/ubuntu/data --auth
 fi
 
 COUNTER=0
 
-while ! nc -z localhost $PORT; do
+while ! nc -z localhost 27018; do
 	echo "it's been $COUNTER seconds and it is not started yet..."
 	let COUNTER=COUNTER+1
   	sleep 1 # wait for 1 second before check again
