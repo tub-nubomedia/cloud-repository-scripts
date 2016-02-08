ddd17b77203144f13359efab7775c4e699ddfb39
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-02-08 18:53:21 +0100

    added floating ip
    to /etc/kurento/kurento-repo.conf.json

--- start-kurento-repo-server.sh
+++ start-kurento-repo-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo sed -i -e "s/127.0.0.1/$internal_nubomedia_floatingIp/g" /etc/kurento/kurento-repo.conf.json
+
 cd /opt/kurento-repository-server-6.3.0
 screen -d -m -S kurento-repo-server bin/start.sh
