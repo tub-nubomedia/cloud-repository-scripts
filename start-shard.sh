07361c0c10f7aa87ca2a56c1cb3074448c639332
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-06-27 15:55:20 +0200

    Update start-shard.sh

--- start-shard.sh
+++ start-shard.sh
@@ -17,7 +17,7 @@ export LC_ALL=C
 
 mkdir -p ~/data/shard
 
-screen -d -m -S shard mongod --smallfiles --port 27017 --dbpath ~/data/shard
+screen -d -m -S shard mongod --smallfiles --port $port --dbpath ~/data/shard
 
 while ! nc -z localhost 27017; do   
   sleep 0.5 # wait for 1/10 of the second before check again
